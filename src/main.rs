8f525debe4a225464c312e8b85ca2daaf9dbf38c
Olivier Lischer <olivier.lischer@liolin.ch>
2021-04-12 16:21:07 +0200

    Add the music player

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ use std::{fs, path::PathBuf};
 use termion::event::Key;
 use tui::widgets::ListState;
 
+use rodio::OutputStreamHandle;
+use rodio::{Decoder, OutputStream, Sink};
+use std::fs::File;
+use std::io::BufReader;
+
 mod model;
 #[cfg(test)]
 mod test_helpers;
@@ -20,6 +25,39 @@ use crate::model::{Album, App, Artist, Track};
 use crate::ui::view::{self, View};
 use crate::ui::widget::StatefulList;
 use crate::util::Events;
+
+struct Player {
+    sink: Sink,
+    stream: OutputStream,
+    stream_handle: OutputStreamHandle,
+}
+
+impl Player {
+    fn new() -> Player {
+        let (stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+        Player {
+            sink,
+            stream,
+            stream_handle,
+        }
+    }
+
+    fn play_new_track(&mut self, path: &str) {
+        if !self.sink.empty() {
+            let (stream, stream_handle) = OutputStream::try_default().unwrap();
+            self.stream = stream;
+            self.stream_handle = stream_handle;
+            self.sink = Sink::try_new(&self.stream_handle).unwrap();
+        }
+
+        let file = BufReader::new(File::open(path).unwrap());
+        let source = Decoder::new(file).unwrap();
+
+        self.sink.append(source);
+        self.sink.play();
+    }
+}
 
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
@@ -49,6 +87,7 @@ async fn main() -> anyhow::Result<()> {
     };
 
     let mut terminal = ui::init_view()?;
+    let mut player = Player::new();
     let events = Events::new();
     terminal.clear()?;
 
@@ -75,7 +114,7 @@ async fn main() -> anyhow::Result<()> {
             Key::Char('\n') => {
                 let selected = app.tracks.state.selected().unwrap();
                 let track = &app.tracks.items[selected];
-                println!("Play {}", track.title)
+                player.play_new_track(&track.file_path);
             }
             _ => {}
         }
